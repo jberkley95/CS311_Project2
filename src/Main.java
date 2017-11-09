1a934fd5f10bec2a704faaf37516467a9b543391
John Berkley <jberkley95@gmail.com>
2017-11-09 00:07:23 -0800

    Finalized project & added documentation

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,42 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * @author John Berkley
  * CPP Class: CS 311
  * Date Created: Nov 07, 2017
+ *
+ * Program was written in Intellij IDEA using JDK 9
+ * Modify keywordScanner to point to wherever the text file containing the keywords text is located
+ * Modify programScanner to point to wherever the text file containing the program text is located
+ * If the program text is too long for the current arrays, modify MAX_TRANSITION in DynamicFSA.java
+ *  to handle a larger number of characters (default is set to 10,000)
  */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        /*Scanner keywordScanner = new Scanner(new File("/Users/johnberkley/IdeaProjects/CS311_Project2/src/Keywords"));
+        Scanner keywordScanner = new Scanner(new File("/Users/johnberkley/IdeaProjects/CS311_Project2/src/Keywords"));
         DynamicFSA dfsa = new DynamicFSA();
 
+        //process each line of the keyword file
         while (keywordScanner.hasNextLine()) {
             dfsa.processLine(keywordScanner.nextLine(), true);
-        }*/
+        }
 
-        String newInput = "print(Arr[0][2]));  a12";
-        newInput = newInput.replaceAll("[^a-zA-Z0-9_$][0-9]*", " ");
-        String[] line = newInput.split("\\s+");
-        System.out.println(Arrays.toString(line));
+        //process each line of the program file (includes some small formatting)
+        Scanner programScanner = new Scanner(new File("/Users/johnberkley/IdeaProjects/CS311_Project2/src/JavaProgram"));
+        while (programScanner.hasNextLine()) {
+            String nextLine = programScanner.nextLine().trim();
+            if (nextLine.equals("")) {
+                System.out.println();
+            }
+            if (!nextLine.equals("") && nextLine.charAt(0) != '/' && !nextLine.trim().equals("{")) {
+                dfsa.processLine(nextLine, false);
+                System.out.println();
+            }
+        }
+
+        //display switch and next tables
+        dfsa.displayTables();
     }
 }
